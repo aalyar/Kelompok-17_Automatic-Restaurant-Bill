f270a8cca309a6b3c23d9a1f3e4233b82fa23cd5
Alya Ramadhani <80261857+aalyar@users.noreply.github.com>
2021-06-18 23:49:01 +0700

    Update restaurantBill.py

--- restaurantBill.py
+++ restaurantBill.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+# Tampilan awal program
 print("""
     ******************************
     Selamat Datang di Restaurant
@@ -24,6 +25,7 @@ print("""
     ==============================
     """)
 
+# Masukkan input dari pelanggan
 pelanggan = str(input("Nama Pelanggan ="))
 nomormeja = int(input("Meja Nomor ="))
 
@@ -34,7 +36,6 @@ buah = 0
 diskon = 0
 ppn1 = 0
 
-
 def makanan():
     global total1
     global jumlahpesan
@@ -42,8 +43,10 @@ def makanan():
     global diskon
     global ppn1
     print("\nMakanan")
+# Masukkan input dari pelanggan
     pesan = str(input("Masukkan Kode Menu ="))
     jumlahpesan = int(input("Jumlah Pesanan ="))
+    # Pilihan menu makanan
     if pesan == "a1":
         menu1 = "Ayam Saus Tiram"
         harga = (30000 * jumlahpesan)
@@ -53,7 +56,7 @@ def makanan():
             total1 = int(harga - diskon + ppn1)
             if jumlahpesan > 10:
                 print("Maaf stok menu yang tersedia hanya 10")
-                return(makanan())
+                return (makanan())
             else:
                 print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
         else:
@@ -122,6 +125,7 @@ makanan()
 total2 = 0
 menu2 = ""
 jumlah = 0
+ppn2 = 0
 
 def minuman():
     global total2
@@ -130,9 +134,11 @@ def minuman():
     global diskon
     global ppn2
     print("\nMinuman")
+
+# Masukkan input pilihan dari pelanggan
     pesan = str(input("Masukkan Kode Menu ="))
     jumlah = int(input("Jumlah Pesanan ="))
-
+# Pilihan menu minuman
     if pesan == "a2":
         menu2 = "Teh/Es"
         harga = (3000 * jumlah)
@@ -188,20 +194,23 @@ def minuman():
         minuman()
 minuman()
 
-totalharga = total1+total2
-ppntotal = ppn1+ppn2
+totalharga = total1+total2 # Menghitung total harga pesanan
+ppntotal = ppn1+ppn2   # Menghitung total PPN
 
+# Mencetak nota sementara
 print("--------------------------")
 print("Pesanan")
 print("--------------------------")
 print("Menu :", menu1, "||", menu2)
-print("Jumlah :",jumlahpesan, "porsi", "||", jumlah, "buah")
+print("Jumlah :", jumlahpesan, "porsi", "||", jumlah, "buah")
 print("Harga :", total1, "+", total2)
 print("Diskon :", diskon)
 print("PPN :", ppntotal)
 print("--------------------------")
 print("Total :", totalharga)
 print("--------------------------")
+
+# Memastikan pesanan
 pastikan_pesanan = str(input("Apakah pesanan Anda sudah sesuai? Silahkan periksa kembali (y/n) ="))
 if pastikan_pesanan == 'y':
     print("Silahkan lanjut ke metode pembayaran")
@@ -225,175 +234,17 @@ else:
         e2. Kopi         : Rp 10.000
         ==============================
         """)
-    total1 = 0
-    menu1 = ""
-    jumlahpesan = 0
-    buah = 0
-    diskon = 0
-    ppn1 = 0
-
-    def makanan():
-        global total1
-        global jumlahpesan
-        global menu1
-        global diskon
-        global ppn1
-        print("\nMakanan")
-        pesan = str(input("Masukkan Kode Menu ="))
-        jumlahpesan = int(input("Jumlah Pesanan ="))
-        if pesan == "a1":
-            menu1 = "Ayam Saus Tiram"
-            harga = (30000 * jumlahpesan)
-            ppn1 = int(harga * 0.1)
-            if jumlahpesan >= 5:
-                diskon = int(harga * 0.2)
-                total1 = int(harga - diskon + ppn1)
-                if jumlahpesan > 10:
-                    print("Maaf stok menu yang tersedia hanya 10")
-                    return (makanan())
-                else:
-                    print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
-            else:
-                total1 = int(harga + ppn1)
-        elif pesan == "b1":
-            menu1 = "Ayam Goreng Mentega"
-            harga = (28000 * jumlahpesan)
-            ppn1 = int(harga * 0.1)
-            if jumlahpesan >= 5:
-                diskon = int(harga * 0.2)
-                total1 = int(harga - diskon + ppn1)
-                if jumlahpesan > 10:
-                    print("Maaf stok menu yang tersedia hanya 10")
-                    return (makanan())
-                else:
-                    print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
-            else:
-                total1 = int(harga + ppn1)
-        elif pesan == "c1":
-            menu1 = "Ayam Goreng Crispy"
-            harga = int(27000 * jumlahpesan)
-            ppn1 = int(harga * 0.1)
-            if jumlahpesan >=5:
-                diskon = int(harga * 0.2)
-                total1 = int(harga - diskon + ppn1)
-                if jumlahpesan > 10:
-                    print("Maaf stok menu yang tersedia hanya 10")
-                    return (makanan())
-                else:
-                    print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
-            else:
-                total1 = int(harga + ppn1)
-        elif pesan == "d1":
-            menu1 = "Ayam Lada Hitam"
-            harga = int(30000 * jumlahpesan)
-            ppn1 = int(harga * 0.1)
-            if jumlahpesan >=5:
-                diskon = int(harga * 0.2)
-                total1 = int(harga - diskon + ppn1)
-                if jumlahpesan > 10:
-                    print("Maaf stok menu yang tersedia hanya 10")
-                    return (makanan())
-                else:
-                    print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
-            else:
-                total1 = int(harga + ppn1)
-        elif pesan == "e1":
-            menu1 = "Nasi Uduk"
-            harga = int(10000 * jumlahpesan)
-            ppn1 = int(harga * 0.1)
-            if jumlahpesan >=5:
-                diskon = int(harga * 0.2)
-                total1 = int(harga - diskon + ppn1)
-                if jumlahpesan > 10:
-                    print("Maaf stok menu yang tersedia hanya 10")
-                    return (makanan())
-                else:
-                    print("Pesanan yang anda pesan sebanyak = ", jumlahpesan)
-            else:
-                total1 = int(harga + ppn1)
-        else:
-            print("Menu tidak terdaftar, silahkan masukkan kembali pesanan anda")
-            makanan()
     makanan()
-
-    total2 = 0
-    menu2 = ""
-    jumlah = 0
-
-
-    def minuman():
-        global total2
-        global menu2
-        global jumlah
-        global diskon
-        global ppn2
-        print("\nMinuman")
-        pesan = str(input("Masukkan Kode Menu ="))
-        jumlah = int(input("Jumlah Pesanan ="))
-
-        if pesan == "a2":
-            menu2 = "Teh/Es"
-            harga = (3000 * jumlah)
-            ppn2 = int(harga * 0.1)
-            total2 = int(harga + ppn2)
-            if jumlah > 10:
-                print("Maaf stok menu yang tersedia hanya 10")
-                return (minuman())
-            else:
-                print("Pesanan yang anda pesan sebanyak = ", jumlah)
-        elif pesan == "b2":
-            menu2 = "Jeruk/Es"
-            harga = int(4000 * jumlah)
-            ppn2 = int(harga * 0.1)
-            total2 = int(harga + ppn2)
-            if jumlah > 10:
-                print("Maaf stok menu yang tersedia hanya 10")
-                return (minuman())
-            else:
-                print("Pesanan yang anda pesan sebanyak = ", jumlah)
-        elif pesan == "c2":
-            menu2 = "Lemon Tea/Es"
-            harga = int(6000 * jumlah)
-            ppn2 = int(harga * 0.1)
-            total2 = int(harga + ppn2)
-            if jumlah > 10:
-                print("Maaf stok menu yang tersedia hanya 10")
-                return (minuman())
-            else:
-                print("Pesanan yang anda pesan sebanyak = ", jumlah)
-        elif pesan == "d2":
-            menu2 = "Susu/Es"
-            harga = int(8000 * jumlah)
-            ppn2 = int(harga * 0.1)
-            total2 = int(harga + ppn2)
-            if jumlah > 10:
-                print("Maaf stok menu yang tersedia hanya 10")
-                return (minuman())
-            else:
-                print("Pesanan yang anda pesan sebanyak = ", jumlah)
-        elif pesan == "e2":
-            menu2 = "Kopi"
-            harga = int(10000 * jumlah)
-            ppn2 = int(harga * 0.1)
-            total2 = int(harga + ppn2)
-            if jumlah > 10:
-                print("Maaf stok menu yang tersedia hanya 10")
-                return (minuman())
-            else:
-                print("Pesanan yang anda pesan sebanyak = ", jumlah)
-        else:
-            print("Menu tidak terdaftar, silahkan masukkan kembali pesanan anda")
-            minuman()
     minuman()
 
-    totalharga = total1 + total2
-    ppntotal = ppn1 + ppn2
+    totalharga = total1 + total2    # Menghitung total harga pesanan
+    ppntotal = ppn1 + ppn2          # Menghitung total PPN
 
 print("--------------------------")
 print("Pesanan")
 print("--------------------------")
 print("Menu :", menu1, "||", menu2)
-print("Jumlah :",jumlahpesan, "porsi", "||", jumlah, "buah")
+print("Jumlah :", jumlahpesan, "porsi", "||", jumlah, "buah")
 print("Harga :", total1, "+", total2)
 print("Diskon :", diskon)
 print("PPN :", ppntotal)
@@ -401,7 +252,7 @@ print("--------------------------")
 print("Total :", totalharga)
 print("--------------------------")
 
-pesan_lagi = str(input("Apakah ingin pesan lagi?(y/n)"))
+pesan_lagi = str(input("Apakah ingin pesan lagi?(y/n)="))    # Memastikan apakah ingin pesan lagi
 if pesan_lagi == "y":
     print("""
             >>>>>>>>>>>> MENU <<<<<<<<<<<<
@@ -437,8 +288,10 @@ if pesan_lagi == "y":
         global diskon
         global ppn3
         print("\nMakanan")
+        # Masukkan input dari user
         pesan = str(input("Masukkan Kode Menu ="))
         jumlahpesanan = int(input("Jumlah Pesanan ="))
+        # Pilihan menu makanan
         if pesan == "a1":
             menu3 = "Ayam Saus Tiram"
             harga = (30000 * jumlahpesanan)
@@ -512,14 +365,12 @@ if pesan_lagi == "y":
         else:
             print("Menu tidak terdaftar, silahkan masukkan kembali pesanan anda")
             makanan()
-
-
     makanan()
 
     total4 = 0
     menu4 = ""
     jumlahminum = 0
-
+    ppn4 = 0
 
     def minuman():
         global total4
@@ -528,9 +379,10 @@ if pesan_lagi == "y":
         global diskon
         global ppn4
         print("\nMinuman")
+        # Masukkan input dari user
         pesan = str(input("Masukkan Kode Menu ="))
         jumlahminum = int(input("Jumlah Pesanan ="))
-
+        # Pilihan menu minuman
         if pesan == "a2":
             menu4 = "Teh/Es"
             harga = (3000 * jumlahminum)
@@ -584,18 +436,17 @@ if pesan_lagi == "y":
         else:
             print("Menu tidak terdaftar, silahkan masukkan kembali pesanan anda")
             minuman()
-
-
     minuman()
 
-    totalharga = total1 + total2 +total3 + total4
-    ppntotal = ppn1 + ppn2 + ppn3 + ppn4
+    totalharga = total1 + total2 + total3 + total4   # Menghitung total harga pesanan
+    ppntotal = ppn1 + ppn2 + ppn3 + ppn4            # Menghitung total PPN
 
+    # Mencetak nota
     print("--------------------------")
     print("Pesanan")
     print("--------------------------")
-    print("Menu :", menu1,"dan", menu3, "||", menu2,"dan", menu4)
-    print("Jumlah :", jumlahpesan,"porsi", "+", jumlahpesanan, "porsi", "||", jumlah,"buah", "+", jumlahminum, "buah")
+    print("Menu :", menu1, "dan", menu3, "||", menu2, "dan", menu4)
+    print("Jumlah :", jumlahpesan, "porsi", "+", jumlahpesanan, "porsi", "||", jumlah, "buah", "+", jumlahminum, "buah")
     print("Harga :", total1, "+", total3, "+", total2, "+", total4)
     print("Diskon :", diskon)
     print("PPN :", ppntotal)
@@ -603,8 +454,10 @@ if pesan_lagi == "y":
     print("Total :", totalharga)
     print("--------------------------")
 
-    print("\nTotal harus Dibayar: ", totalharga)
+    print("\nTotal harus Dibayar: ", totalharga)       # Mencetak total pesanan
+    # Masukkan input dari user
     metode_pembayaran = str(input("Melakukan pembayaran melalui (Cash/Kredit/Ovo/Go-Pay) = "))
+    # Pilihan metode pembayaran
     if metode_pembayaran == 'cash':
         print("Silahkan menuju kasir dan melakukan pembayaran")
     elif metode_pembayaran == 'kredit':
@@ -628,21 +481,22 @@ if pesan_lagi == "y":
     else:
         print("Maaf pesanan anda dibatalkan")
         exit()
+    # Input pembayaran dari user
     bayar = int(input("Pelanggan membayar sebesar :"))
     if bayar < totalharga:
         print("Maaf uang yang anda masukkan kurang, Silahkan masukkan kembali")
         bayar = int(input("Pelanggan membayar sebesar :"))
-    kembalian = int(bayar - totalharga)
+    kembalian = int(bayar - totalharga)     # Mengecek apakah pembayaran ada kembalian
 
     waktu = datetime.now()
-    #Open file txt
+    # Open file txt
     print("\n=============================================================")
     print("                           STRUK                             ")
     print("=============================================================")
-    print("                         ",waktu)
+    print("             ", waktu)
     print(" Nama                :", pelanggan)
     print(" Meja                :", nomormeja)
-    print(" Pesanan             :" ,jumlahpesan, menu1, menu3, "dan", jumlah, menu2, menu4)
+    print(" Pesanan             :", jumlahpesan, menu1, menu3, "dan", jumlah, menu2, menu4)
     print(" Total               : Rp.", totalharga)
     print(" Metode Pembayaran   :", metode_pembayaran)
     print(" Nominal Bayar       : Rp.", bayar)
@@ -651,18 +505,20 @@ if pesan_lagi == "y":
     print("                        TERIMA KASIH                          ")
     print("==============================================================")
 
+    # Mencetak struk final
     struk = "\n=============================================================" \
             "\n                           STRUK                             " \
             "\n=============================================================" \
-            "\n                                   {}".format(waktu)
-    struk1 = "\nNama: {}\nMeja: {}\nJumlah Pesan Makanan: {}\nMenu Makanan: {}\nJumlah Pesan Minuman: {}\nMenu Minuman: {}\nTotal: {}\nMetode Pembayaran: {}\nNominal Bayar: {}\nKembalian: {}".format(pelanggan, nomormeja,jumlahpesan, menu1, jumlah, menu2, totalharga, metode_pembayaran, bayar, kembalian)
+            "\n                   {}".format(waktu)
+    struk1 = "\nNama: {}\nMeja: {}\nJumlah Pesan Makanan: {}\nMenu Makanan: {}\nJumlah Pesan Minuman: {}\nMenu Minuman: {}\nTotal: {}\nMetode Pembayaran: {}\nNominal Bayar: {}\nKembalian: {}".format(pelanggan, nomormeja, jumlahpesan, menu1, jumlah, menu2, totalharga, metode_pembayaran, bayar, kembalian)
     struk2 = "\n=============================================================" \
              "\n                        TERIMA KASIH                         " \
              "\n============================================================="
-
 else:
     print("\nTotal harus Dibayar: ", totalharga)
+    # Masukkan input dari user
     metode_pembayaran = str(input("Melakukan pembayaran melalui (Cash/Kredit/Ovo/Go-Pay) = "))
+    # Pilihan metode pembayaran
     if metode_pembayaran == 'cash':
         print("Silahkan menuju kasir dan melakukan pembayaran")
     elif metode_pembayaran == 'kredit':
@@ -680,7 +536,7 @@ else:
     else:
         print("maaf metode pembayaran tidak tersedia")
         exit()
-    pasti_bayar = str(input("Apakah sudah melakukan pembayaran? (y/n) = "))
+    pasti_bayar = str(input("Apakah sudah melakukan pembayaran? (y/n) = "))  # Memastikan pembayaran telah dilakukan
     if pasti_bayar == 'y':
         print("Silahkan lanjut")
     else:
@@ -690,17 +546,17 @@ else:
     if bayar < totalharga:
         print("Maaf uang yang anda masukkan kurang, Silahkan masukkan kembali")
         bayar = int(input("Pelanggan membayar sebesar :"))
-    kembalian = int(bayar - totalharga)
+    kembalian = int(bayar - totalharga)     # Mengecek apakah pembayaran ada kembalian
 
     waktu = datetime.now()
-    #Open file txt
+    # Open file txt
     print("\n=============================================================")
     print("                           STRUK                             ")
     print("=============================================================")
-    print("                         ",waktu)
+    print("                         ", waktu)
     print(" Nama                :", pelanggan)
     print(" Meja                :", nomormeja)
-    print(" Pesanan             :" ,jumlahpesan, menu1, "dan", jumlah, menu2)
+    print(" Pesanan             :", jumlahpesan, menu1, "dan", jumlah, menu2)
     print(" Total               : Rp.", totalharga)
     print(" Metode Pembayaran   :", metode_pembayaran)
     print(" Nominal Bayar       : Rp.", bayar)
@@ -709,14 +565,15 @@ else:
     print("                        TERIMA KASIH                          ")
     print("==============================================================")
 
+    # Mencetak struk final
     struk = "\n=============================================================" \
             "\n                           STRUK                             " \
             "\n=============================================================" \
-            "\n                                   {}".format(waktu)
-    struk1 = "\nNama: {}\nMeja: {}\nJumlah Pesan Makanan: {}\nMenu Makanan: {}\nJumlah Pesan Minuman: {}\nMenu Minuman: {}\nTotal: {}\nMetode Pembayaran: {}\nNominal Bayar: {}\nKembalian: {}".format(pelanggan, nomormeja,jumlahpesan, menu1, jumlah, menu2, totalharga, metode_pembayaran, bayar, kembalian)
+            "\n                  {}".format(waktu)
+    struk1 = "\nNama: {}\nMeja: {}\nJumlah Pesan Makanan: {}\nMenu Makanan: {}\nJumlah Pesan Minuman: {}\nMenu Minuman: {}\nTotal: {}\nMetode Pembayaran: {}\nNominal Bayar: {}\nKembalian: {}".format(pelanggan, nomormeja, jumlahpesan, menu1, jumlah, menu2, totalharga, metode_pembayaran, bayar, kembalian)
     struk2 = "\n=============================================================" \
-            "\n                        TERIMA KASIH                         " \
-            "\n============================================================="
+             "\n                        TERIMA KASIH                         " \
+             "\n============================================================="
 
 file_data = open("struk.txt", "a")
 file_data.write(struk)
